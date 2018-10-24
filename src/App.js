9e73f54b523e991d99a8c6bf376b6d98fdff3e7c
Audrey Mertens <audrey.mertens@hotmail.be>
2018-10-24 17:53:11 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+// src/App.js
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Travel from "./Travel";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Simpsons Quotes</h1>
+        </header>
+        <Travel />
+        <Travel
+          destination="Lisbonne"
+          country="Portugal"
+          photo="https://i.goopics.net/wL3O9.jpg"
+          distance="1,585 km"
+        />
+        <Travel
+          destination="Namur"
+          country="belgium"
+          photo="https://i.goopics.net/dkm1y.jpg"
+          distance="69 km"
+        />
+      </div>
+    );
+  }
+}
+
+export default Travel;
